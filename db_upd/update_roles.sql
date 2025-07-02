bf64698c10b79749150b8498946108899ecf877f
Christian Mulato <chmulato@hotmail.com>
2025-07-02 07:48:39 -0300

    feat: Remove outdated password update scripts and roles management SQL files

--- db_upd/update_roles.sql
+++
@@ -1,4 +0,0 @@
--- Script para atualizar as roles no banco de dados, adicionando o prefixo ROLE_ quando necessário
-UPDATE usuario SET role = 'ROLE_ADMIN' WHERE role = 'ADMIN';
-UPDATE usuario SET role = 'ROLE_DENTIST' WHERE role = 'DENTIST';
-UPDATE usuario SET role = 'ROLE_RECEPTIONIST' WHERE role = 'RECEPTIONIST';
