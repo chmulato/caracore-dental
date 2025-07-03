73ef9a2dbdf740ab585a97959ed50534249f8823
Christian Mulato <chmulato@hotmail.com>
2025-07-03 11:35:56 -0300

    feat: Update test data to include status, creation date, and duration for agendamentos

--- src/test/resources/db/testdata.sql
+++ src/test/resources/db/testdata.sql
@@ -6,8 +6,8 @@ INSERT INTO usuario (email, nome, senha, role)
 VALUES ('admin@teste.com', 'Administrador de Testes', '$2a$10$ktLieeeNJAD9iA5l8VsR6..erCGtsqwWFm57vspe.wsxCT9FDTiXy', 'ROLE_ADMIN');
 
 -- Inserir alguns agendamentos para testes
-INSERT INTO agendamento (data_hora, paciente, dentista)
-VALUES ('2023-12-01 10:00:00', 'Paciente Teste 1', 'Dentista Teste');
+INSERT INTO agendamento (data_hora, paciente, dentista, status, data_criacao, duracao_minutos)
+VALUES ('2023-12-01 10:00:00', 'Paciente Teste 1', 'Dentista Teste', 'AGENDADO', CURRENT_TIMESTAMP, 60);
 
-INSERT INTO agendamento (data_hora, paciente, dentista)
-VALUES ('2023-12-01 14:30:00', 'Paciente Teste 2', 'Dentista Teste');
+INSERT INTO agendamento (data_hora, paciente, dentista, status, data_criacao, duracao_minutos)
+VALUES ('2023-12-01 14:30:00', 'Paciente Teste 2', 'Dentista Teste', 'AGENDADO', CURRENT_TIMESTAMP, 30);
