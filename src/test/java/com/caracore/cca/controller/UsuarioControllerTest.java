84994e32d69152185747f02dbf51db44e5de17ce
Christian Mulato <chrvlamulato@rd.com.br>
2025-07-05 19:33:09 -0300

    test: Add unit tests for UsuarioController to enhance coverage and reliability

--- src/test/java/com/caracore/cca/controller/UsuarioControllerTest.java
+++ src/test/java/com/caracore/cca/controller/UsuarioControllerTest.java
@@ -21,7 +21,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
