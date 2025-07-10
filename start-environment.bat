768f7c0cde010c910331f5f938eadeec14939339
Christian Mulato <chmulato@hotmail.com>
2025-07-10 20:55:37 -0300

    feat: Remove convenience scripts for starting CCA environment

--- start-environment.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-:: Script de conveniência para iniciar o ambiente CCA
-:: Chama o script completo localizado na pasta scripts
-
-echo Iniciando ambiente CCA...
-echo.
-call "%~dp0scripts\start-docker.bat"
